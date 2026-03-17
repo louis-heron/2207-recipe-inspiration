d11a1e862eef83eeeb3217184e207b8563a77dbb
pauline le biez <pauline@onemeteraway.com>
2026-03-17 15:33:35 +0100

    adding the corrected code

--- api/Vision/detector.py
+++ api/Vision/detector.py
@@ -1,14 +1,19 @@
 from ultralytics import YOLO
 from pathlib import Path
 
+import io
+from PIL import Image
+
 model_path = "Vision/best.pt"
 
 class IngredientDetector:
     def __init__(self):
         self.model = YOLO(model_path)
+            # Convert bytes to a PIL Image
 
     def detect(self, image_bytes):
         # Run results
+        image = Image.open(io.BytesIO(image_bytes))
         results = self.model(image_bytes)
 
         # Extract unique class names detected
